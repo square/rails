2965e2645ebe3730062a2a400dcebfc0a052a56f
Erich Menge <erich.menge@me.com>
2012-05-29 16:40:43 -0500

    True, False, and Nil should be represented in as_json as themselves.
    Conflicts:
    	activesupport/lib/active_support/json/encoding.rb

--- activesupport/lib/active_support/json/encoding.rb
+++ activesupport/lib/active_support/json/encoding.rb
@@ -161,18 +161,18 @@ class Struct
 end
 
 class TrueClass
-  AS_JSON = ActiveSupport::JSON::Variable.new('true').freeze
-  def as_json(options = nil) AS_JSON end #:nodoc:
+  def as_json(options = nil) self end #:nodoc:
+  def encode_json(encoder) to_s end #:nodoc:
 end
 
 class FalseClass
-  AS_JSON = ActiveSupport::JSON::Variable.new('false').freeze
-  def as_json(options = nil) AS_JSON end #:nodoc:
+  def as_json(options = nil) self end #:nodoc:
+  def encode_json(encoder) to_s end #:nodoc:
 end
 
 class NilClass
-  AS_JSON = ActiveSupport::JSON::Variable.new('null').freeze
-  def as_json(options = nil) AS_JSON end #:nodoc:
+  def as_json(options = nil) self end #:nodoc:
+  def encode_json(encoder) 'null' end #:nodoc:
 end
 
 class String
