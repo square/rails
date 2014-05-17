06aeb60560b6e7998fac84b329d0ec20409b6d16
Tamir Duberstein <tamird@squareup.com>
2014-05-16 21:46:53 -0700

    Fix flash the third

--- actionpack/lib/action_dispatch/middleware/flash.rb
+++ actionpack/lib/action_dispatch/middleware/flash.rb
@@ -72,7 +72,7 @@ module ActionDispatch
       attr_writer :used
 
       def self.from_h(h)
-        new.replace(Hash[h].except(USED_KEY)).tap do |flash|
+        new.replace(h.except(USED_KEY)).tap do |flash|
           flash.used = Set.new(h[USED_KEY])
         end if h
       end
@@ -192,7 +192,7 @@ module ActionDispatch
 
     def call(env)
       if (session = env['rack.session']) && (flash = session['flash'])
-        swept_flash = Flash::FlashHash.from_h(flash).tap(&:sweep)
+        swept_flash = Flash::FlashHash.from_h(Hash[flash]).tap(&:sweep)
         session['flash'] = swept_flash
       end
 
