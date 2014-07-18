5cb48d9cb8c28e4db31fe1e66d7e1d7179827451
Gabriel Gilder <gabriel@squareup.com>
2014-07-18 11:40:57 -0700

    Backport support for Mysql SSL from 4.0

--- railties/lib/rails/commands/dbconsole.rb
+++ railties/lib/rails/commands/dbconsole.rb
@@ -68,6 +68,11 @@ module Rails
           'socket'    => '--socket',
           'username'  => '--user',
           'encoding'  => '--default-character-set'
+          'sslca'     => '--ssl-ca',
+          'sslcert'   => '--ssl-cert',
+          'sslcapath' => '--ssl-capath',
+          'sslcipher' => '--ssh-cipher',
+          'sslkey'    => '--ssl-key'
         }.map { |opt, arg| "#{arg}=#{config[opt]}" if config[opt] }.compact
 
         if config['password'] && include_password
